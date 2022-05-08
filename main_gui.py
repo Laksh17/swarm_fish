abea709f8ece06b0eded21b513faf1d532e412b9
Laksh <ssglaksh17@gmail.com>
2022-05-08 11:27:54 +0530

    Init main_gui.py file (no coords)

---
+++ main_gui.py
@@ -0,0 +1,60 @@
+from cgitb import reset
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from matplotlib.pyplot import figure
+import numpy as np
+import time
+
+root = tk.Tk()
+root.title('Basic Layout of Water Body')
+root.configure(background='#232343')
+root.geometry("700x600")
+
+fig = Figure()
+ax = fig.add_subplot(111)
+
+ax.set_title("Water area")
+ax.set_xlabel("X coords")
+ax.set_ylabel("Y coords")
+ax.set_xlim(0, 700)
+ax.set_ylim(0, 700)
+lines = ax.plot([], [])[0]
+
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.get_tk_widget().place(x=10, y=10, width=680, height=450)
+canvas.draw()
+
+# create start and stop buttons
+root.update()
+start = tk.Button(root, text='Start', font=(
+    'calbiri', 13), command=lambda: plot_start())
+start.place(x=260, y=500)
+
+root.update()
+stop = tk.Button(root, text='Stop', font=(
+    'calbiri', 13), command=lambda: plot_stop())
+stop.place(x=start.winfo_x()+start.winfo_reqwidth()+20, y=500)
+
+root.update()
+reset = tk.Button(root, text='Reset', font=(
+    'calbiri', 13), command=lambda: plot_reset())
+reset.place(x=stop.winfo_x()+start.winfo_reqwidth()+20, y=500)
+
+# start
+
+
+# adding points to the plot
+def plot_start():
+    print("Plot started")
+
+
+def plot_stop():
+    print("Plot stopped")
+
+
+def plot_reset():
+    print("Plot has been reset")
+
+
+root.mainloop()

49da64123902b1da93a9dc5945e6d945cecba3d9
Laksh <ssglaksh17@gmail.com>
2022-06-08 23:22:00 +0530

    First release of the synchronous ordered algorithm

--- src/script.js
+++ src/script.js
@@ -25,6 +25,12 @@ let canvasPosition = canvas.getBoundingClientRect();
 let fishyArray = [];
 const generateButton = document.getElementById("generateFish");
 console.log(canvasPosition.left);
+
+function* range(start, end) {
+    for (let i = start; i <= end; i++) {
+        yield i;
+    }
+}
 //Add the fish object
 
 class Fish {
@@ -35,53 +41,97 @@ class Fish {
     constructor(number) {
         // this.x = 50 + 550;
         Fish.count();
+        this.isPaused = false;
         this.radius = 15;
-        this.x = 15;
-        this.y = 325;
+        this.x = this.radius;
+        this.y = canvasPosition.height / 2;
         this.section = number;
         this.frameX = 0;
         this.frameY = 0; //frames of sprite sheet
         this.frame = 0;
-        this.targetX = 15;
-        this.targetY = 325;
+        this.gap = 10;
+        this.targetX_start = this.radius;
+        this.targetX_end = canvasPosition.width - this.radius;
+        this.targetY_start = canvasPosition.height / 2;
+        this.targetY_end = canvasPosition.height / 2;
+        this.approxX = 1.5;
+        this.approxY = 1.5;
+        this.lastPath = false;
+        this.reached = false;
         // this.spriteHeight = 327;
         // this.spriteWidth = 498;
     }
-    deploy() {
-
-    }
-    pathSetup() {
-
-    }
+    // deploy(totalFish) {
+    //     this.isPaused = true;
+    //     this.targetY_start = (this.section - 1) * (canvasPosition.height / totalFish) + (this.gap + this.radius);
+    //     this.targetY_end = this.section * (canvasPosition.height / totalFish);
+    //     while ((this.y - this.targetY_start > this.approxY) || (this.y - this.targetY_start < this.approxY)) {
+    //         this.y -= (this.y - this.targetY_start) / 60;
+    //         this.x -= (this.x - this.targetX_start) / 60;
+    //     }
+    // }
     update() {
-        // console.log("Function updated");
-        let rad = this.radius;
+        console.log("Function updated");
         let slot_end = this.section;
         let slot_start = this.section - 1;
-        const dx = this.x - this.targetX;
-        const dy = this.y - this.targetY;
-        const apporxX = 6;
-        const apporxY = 6;
-        console.log("dy at first = " + dy);
-        if (dx >= apporxX || dx <= -apporxX) {
-            this.x -= dx / 60;
-            console.log("dx = " + dx);
-        }
-        if (dy >= apporxY || dy <= -apporxY) {
-            this.y -= dy / 60;
-            console.log("dy = " + dy);
+        // const dx = this.x - this.targetX_end;
+        // const dy = this.y - this.targetY_end;
+        const apporxX = 3;
+        const apporxY = 3;
+        this.targetY_start = slot_start * (canvasPosition.height / count) + this.radius;
+        this.targetY_end = slot_end * (canvasPosition.height / count) - this.radius;
 
+        if (this.reached == false) {
+            const dx = this.x - this.targetX_start;
+            const dy = this.y - this.targetY_start;
+            if (dx > apporxX || dx < -apporxX) {
+                this.x -= dx / 60;
+                console.log("dx = " + dx);
+            }
+            if (dy > apporxY || dy < -apporxY) {
+                this.y -= dy / 60;
+                console.log("dy = " + dy);
+            }
+            if ((Math.abs(dx) <= apporxX) && (Math.abs(dy) <= apporxY)) {
+                this.reached = true;
+                // this.targetX_end = rad + Math.floor(Math.random() * (canvasPosition.width - 2 * rad));
+                // // this.targetX = 400;
+                // this.targetY_end = (slot_start * canvasPosition.height / count) + Math.floor(Math.random() * ((canvasPosition.height / count) - (2 * rad))) + rad;
+                // // this.targetY = 400;
+                // // console.log("ran");
+                console.log("Third IF ran");
+                console.log("This is target" + this.targetY_end);
+                console.log("slot start " + slot_start + " and slot end: " + slot_end + " and count is " + count);
+            }
         }
-        if (dx < apporxX && dy < apporxY) {
-            this.targetX = rad + Math.floor(Math.random() * (canvasPosition.width - 2 * rad));
-            // this.targetX = 400;
-            this.targetY = (slot_start * canvasPosition.height / count) + Math.floor(Math.random() * ((canvasPosition.height / count) - rad)) + rad;
-            // this.targetY = 400;
-            // console.log("ran");
-            console.log("This is target" + this.targetY);
-            console.log("slot start " + slot_start + " and slot end: " + slot_end + " and count is " + count);
-        }
+        else {
+            let dx = this.x - this.targetX_end;
+            console.log(dx);
+            // dy = this.y - this.targetY_end;
+            if (dx != 0) {
+                if (dx < 0) {
+                    this.x += 2;
+                }
+                else {
+                    this.x -= 2;
+                }
+            }
+            else {
 
+                this.targetX_end = this.targetX_start + this.targetX_end;
+                this.targetX_start = this.targetX_end - this.targetX_start;
+                this.targetX_end = this.targetX_end - this.targetX_start;
+                if (this.y + (2 * (this.radius + this.gap)) > this.targetY_end) {
+                    this.reached = false;
+                    console.log("Change of Y ran");
+                }
+                else {
+                    this.y = this.y + (this.radius + this.gap);
+                    console.log("Change of X ran");
+                }
+
+            }
+        }
     }
     draw() {
         // console.log("Function drew");
@@ -118,7 +168,6 @@ animate();
 //     }
 // }
 generateButton.onclick = () => {
-
     fishyArray.push(new Fish(count + 1));
     console.log("ran");
     count++;

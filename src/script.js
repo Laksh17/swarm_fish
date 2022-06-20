90dfdd53562da3832e318662fb97fc8f9ef722bb
Laksh <ssglaksh17@gmail.com>
2022-06-20 07:55:42 +0530

    Fishes move in groups of 4 covering 4 quadrants now

--- src/script.js
+++ src/script.js
@@ -1,50 +1,25 @@
-//To consider for the movement algorithm
-//1) Number of fish present and increase/decrease to that number
-//2) Interaction between fishes
-//Extras:
-//3) Tracking battery level and making the fish move to the shore when running low on battery
-
-
-//To consider for the detection algorithm
-//1) Color of the object should be different
-//2) Store the coordinates at the time of object detection
-//3) Alert user of dead/damaged robo fishes
-
-//Algo Laksh is choosing - line path algo;
-
-
-
 //Basic Canvas Setup
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 850;
 canvas.height = 700;
+const fishRadius = 38
 
+let deployedState = false;
 let count = 0;
 let gameframe = 0;
+
 let canvasPosition = canvas.getBoundingClientRect();
 let fishyArray = [];
 let intruderArray = [];
 let shrimpArray = [];
-const generateFishButton = document.getElementById("generateFish");
+const deployFishButton = document.getElementById("deployFish");
 const clearFishButton = document.getElementById("clearFish");
 const generateIntruderButton = document.getElementById("generateIntruder");
 const clearIntruderButton = document.getElementById("clearIntruder");
 const generateShrimpButton = document.getElementById("generateShrimp");
 const clearShrimpButton = document.getElementById("clearShrimp");
-console.log(canvasPosition.left);
-
-function* range(start, end) {
-    for (let i = start; i <= end; i++) {
-        yield i;
-    }
-}
-
-function arrayRemove(arr, value) {
-    return arr.filter(function (ele) {
-        return ele != value;
-    });
-}
+console.log(canvas.height);
 
 //Add the fish object
 const fishLeft = new Image();
@@ -59,28 +34,39 @@ const shrimpLeft = new Image();
 shrimpLeft.src = 'Prawn_Sprite.png';
 const shrimpRight = new Image();
 shrimpRight.src = 'Prawn_Sprite_Flip.png';
-class Fish {
-    static count() {
-        Fish.counter = (Fish.counter || 0) + 1;
-        return;
+
+//Create a class for quadrants to be chosen
+//Should accept details about all their 4 points which will be given via another function
+//Assign them some numbers
+//Allot each spawned fish with one of those numbers
+
+//Add the fish object
+class Quadrant {
+    constructor(xStart, yStart, xEnd, yEnd) {
+        this.xStart = xStart;
+        this.yStart = yStart;
+        this.xEnd = xEnd;
+        this.yEnd = yEnd;
+        this.occupied = false;
     }
-    constructor(number) {
+}
+
+class Fish {
+    constructor(radius_num, x1, y1, x2, y2) {
         // this.x = 50 + 550;
-        Fish.count();
         this.isPaused = false;
-        this.radius = 38;
+        this.radius = radius_num;
         this.x = this.radius;
-        this.y = canvasPosition.height / 2;
-        this.section = number;
+        this.y = canvas.height / 2;
         this.frameX = 0;
         this.frameY = 0; //frames of sprite sheet
         this.frame = 0;
         this.angle = 0;
         this.gap = 10;
-        this.targetX_start = this.radius;
-        this.targetX_end = canvasPosition.width - this.radius;
-        this.targetY_start = canvasPosition.height / 2;
-        this.targetY_end = canvasPosition.height / 2;
+        this.targetX_start = x1;
+        this.targetX_end = x2;
+        this.targetY_start = y1;
+        this.targetY_end = y2;
         this.approxX = 1.5;
         this.approxY = 1.5;
         this.lastPath = false;
@@ -91,18 +77,18 @@ class Fish {
     }
 
     update() {
-        let slot_end = this.section;
-        let slot_start = this.section - 1;
+        // console.log(3);
         const apporxX = 3;
         const apporxY = 3;
-        this.targetY_start = slot_start * (canvasPosition.height / count) + this.radius;
-        this.targetY_end = slot_end * (canvasPosition.height / count) - this.radius;
-
         if (this.reached == false && this.batteryLevel > 15) {
+            console.log("IN first else");
             const dx = this.x - this.targetX_start;
             const dy = this.y - this.targetY_start;
             let theta = Math.atan2(dy, dx);
             this.angle = theta;
+            console.log("dx = " + dx);
+            console.log("dy = " + dy);
+
             if (dx > apporxX || dx < -apporxX) {
                 this.x -= dx / 60;
                 console.log("dx = " + dx);
@@ -115,16 +101,15 @@ class Fish {
                 this.reached = true;
                 console.log("Third IF ran");
                 console.log("This is target" + this.targetY_end);
-                console.log("slot start " + slot_start + " and slot end: " + slot_end + " and count is " + count);
             }
 
         }
         else if (this.reached == true && this.batteryLevel > 15) {
             let dx = this.x - this.targetX_end;
-
+            console.log("IN second else");
             console.log(dx);
             // dy = this.y - this.targetY_end;
-            if (dx != 0) {
+            if (Math.abs(dx) > this.approxX) {
                 if (dx < 0) {
                     this.x += 2;
                     let theta = Math.atan2(0, dx);
@@ -162,8 +147,10 @@ class Fish {
                 }
 
             }
+
         }
         else {
+            console.log("IN third else");
             let dx;
             let dy;
             if (this.x > (canvas.width / 2)) {
@@ -221,11 +208,7 @@ class Fish {
             ctx.font = "20px Verdana";
             ctx.fillText(this.batteryLevel + "%", this.x - 16, this.y + 53);
         }
-        // let dx = this.x - this.targetX_end;
-        // let dy = this.y - this.targetY_end;
-        // let theta = Math.atan2(dy, dx);
-        // this.angle = theta;
-        // ctx.fillRect(this.x, this.y, this.radius, 10);
+
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle);
@@ -255,147 +238,42 @@ class Fish {
     }
 }
 
-class Intruder {
-    constructor(locationX, locationY) {
-        this.x = locationX;
-        this.y = locationY;
-        this.radius = 27;
-        this.angle = 0;
-        this.frameX = 0;
-        this.frameY = 0;
-        this.spriteHeight = 397;
-        this.spriteWidth = 418;
-        this.targetX_end = locationX;
-        this.targetY_end = locationY;
-    }
-    update() {
-        let dx = this.x - this.targetX_end;
-        let dy = this.y - this.targetY_end;
-        let theta = Math.atan2(dy, dx);
-        this.angle = theta;
-        console.log(dx, dy);
-        if (dx > 0) {
-            this.x--;
-        }
-        else if (dx < 0) {
-            this.x++;
-        }
-        if (dy > 0) {
-            this.y--;
-        }
-        else if (dy < 0) {
-            this.y++;
-        }
-        if (dx == 0 && dy == 0) {
 
-            this.targetX_end = Math.floor((Math.random() * (canvas.width - (2 * this.radius))) + this.radius);
-            this.targetY_end = Math.floor((Math.random() * (canvas.height - (2 * this.radius))) + this.radius);
-        }
-    }
-    draw() {
-        // console.log("Function drew");
-        ctx.fillStyle = 'rgba(255,255,255,0.0)';
-        ctx.lineWidth = 10;
-        ctx.strokeStyle = "#FF0000";
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.angle);
-        let dx = this.x - this.targetX_end;
 
-        if (dx <= 0) {
-            ctx.drawImage(intruderRight, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, 0 - 25, 0 - 25, this.spriteWidth / 8, this.spriteHeight / 8);
-        }
-        else {
-            ctx.drawImage(intruderLeft, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, 0 - 25, 0 - 25, this.spriteWidth / 8, this.spriteHeight / 8);
-        }
-        ctx.restore();
-        // ctx.fillRect(this.x, this.y, this.radius, 10);
-    }
-}
 
-class Shrimp {
-    constructor(locationX, locationY) {
-        this.x = locationX;
-        this.y = locationY;
-        this.radius = 10;
-        this.frameX = 0;
-        this.frameY = 0;
-        this.spriteWidth = 205;
-        this.spriteHeight = 189;
-        this.targetX_end = locationX;
-        this.targetY_end = locationY;
-    }
-    update() {
-        let dx = this.x - this.targetX_end;
-        let dy = this.y - this.targetY_end;
-        console.log(dx, dy);
-        let theta = Math.atan2(dy, dx);
-        this.angle = theta;
-        console.log(dx, dy);
-        if (dx > 0) {
-            this.x -= 0.25;
-        }
-        else if (dx < 0) {
-            this.x += 0.25;
-        }
-        if (dy > 0) {
-            this.y -= 0.25;
-        }
-        else if (dy < 0) {
-            this.y += 0.25;
-        }
-        if (dx == 0 && dy == 0) {
+quadrantTopLeft = new Quadrant(fishRadius, fishRadius, (canvas.width / 2) - fishRadius, (canvas.height / 2) - fishRadius);
+quadrantTopRight = new Quadrant((canvas.width / 2) + fishRadius, fishRadius, canvas.width - fishRadius, (canvas.height / 2) - fishRadius);
+quadrantBottomLeft = new Quadrant(fishRadius, (canvas.height / 2) + fishRadius, (canvas.width / 2) - fishRadius, canvas.height - fishRadius);
+quadrantBottomRight = new Quadrant((canvas.width / 2) + fishRadius, (canvas.height / 2) + fishRadius, canvas.width - fishRadius, canvas.height - fishRadius);
 
-            this.targetX_end = Math.floor((Math.random() * (canvas.width - (2 * this.radius))) + this.radius);
-            this.targetY_end = Math.floor((Math.random() * (canvas.height - (2 * this.radius))) + this.radius);
-        }
-    }
-    draw() {
-        // console.log("Function drew");
-        ctx.fillStyle = 'rgba(255,255,255,0.0)';
-        ctx.lineWidth = 10;
-        ctx.strokeStyle = "#FF0000";
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.angle);
-        let dx = this.x - this.targetX_end;
+quadrantArray = [quadrantTopLeft, quadrantTopRight, quadrantBottomLeft, quadrantBottomRight];
 
-        if (dx <= 0) {
-            ctx.drawImage(shrimpRight, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, 0 - 30, 0 - 20, this.spriteWidth / 4, this.spriteHeight / 4);
-        }
-        else {
-            ctx.drawImage(shrimpLeft, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, 0 - 30, 0 - 20, this.spriteWidth / 4, this.spriteHeight / 4);
-        }
-        ctx.restore();
-
-        // ctx.fillRect(this.x, this.y, this.radius, 10);
-    }
-}
-
-console.log(Fish.counter);
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#233232";
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.stroke();
+    ctx.fillStyle = "#233232";
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height / 2);
+    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.stroke();
     for (let c of fishyArray) {
         c.update();
         c.draw();
-        c.checkIntruder();
+
     }
-    for (let i of intruderArray) {
-        i.update();
-        i.draw();
-    }
-    for (let j of shrimpArray) {
-        j.update();
-        j.draw();
-    }
+    console.log(1);
+    // for (let i of intruderArray) {
+    //     i.update();
+    //     i.draw();
+    // }
+    // for (let j of shrimpArray) {
+    //     j.update();
+    //     j.draw();
+    // }
     requestAnimationFrame(animate);
 }
 animate();
@@ -406,32 +284,12 @@ setInterval(() => {
     }
 }, 1000);
 
-
-generateFishButton.onclick = () => {
-    fishyArray.push(new Fish(count + 1));
-    console.log("ran");
-    count++;
-}
-
-clearFishButton.onclick = () => {
-    fishyArray.pop();
-    count--;
-}
-
-generateIntruderButton.onclick = () => {
-    intruderArray.push(new Intruder(Math.floor((Math.random() * (canvasPosition.width - (2 * 15))) + 15), (Math.floor(Math.random() * (canvasPosition.height - (2 * 15))) + 15)))
-    console.log("Intruder created");
-}
-
-clearIntruderButton.onclick = () => {
-    intruderArray.pop();
-}
-
-generateShrimpButton.onclick = () => {
-    shrimpArray.push(new Shrimp(Math.floor((Math.random() * (canvasPosition.width - (2 * 15))) + 15), Math.floor((Math.random() * (canvasPosition.height - (2 * 15))) + 15)))
-    console.log("Shrimp created");
-}
-
-clearShrimpButton.onclick = () => {
-    shrimpArray.pop();
+deployFishButton.onclick = () => {
+    if (deployedState != true) {
+        for (let i of quadrantArray) {
+            fishyArray.push(new Fish(fishRadius, i.xStart, i.yStart, i.xEnd, i.yEnd));
+            console.log(i.xStart)
+        }
+        deployedState = true;
+    }
 }
